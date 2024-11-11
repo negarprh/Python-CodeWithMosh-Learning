41af9f4bbdbb7fc435c3b73c3042e37ff3f9d9f4
negarprh <negarpr@hotmail.com>
2024-11-11 17:43:59 -0500

    DSA Exersice

--- Data Structures/ds_exersice.py
+++ Data Structures/ds_exersice.py
@@ -1,1 +1,11 @@
 sentence = "This is a common interview question"
+sentence_dict = {}
+
+for word in sentence:
+    if word in sentence_dict:
+        sentence_dict[word] += 1
+    else:
+        sentence_dict[word] = 1
+
+print(f"All the words count{sentence_dict}")
+print(max(sentence_dict))
