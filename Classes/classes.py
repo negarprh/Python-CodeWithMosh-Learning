b0d2d29fc2ff05366f54b1b8e3c3c5688bfe6fc3
negarprh <negarpr@hotmail.com>
2024-12-07 17:37:33 -0500

    Classes

---
+++ Classes/classes.py
@@ -0,0 +1,6 @@
+# Class is a blue print for creating new objects
+# Object: is a instance of a class
+# Each object we have in python is created using a class which is a blue print for creting objects
+
+# Class: Human
+# Objects: John, Maty, Jack
---
+++ Classes/creating_classes.py
@@ -0,0 +1,14 @@
+# Create a point Class
+
+# Naming of classes: each word is starting with Capital letter and there is no _
+
+class Point:
+    # In this block we define all the function that are related to point
+    # For example: draw point, move point or get distance between two point
+
+    def draw(self):
+        print("draw")
+
+
+point = Point()
+point.
