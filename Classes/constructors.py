df3e7234e2f4e647a4bb48b04b5e7ee46762b91b
negarprh <negarpr@hotmail.com>
2024-12-14 21:18:22 -0500

    Start constructors

---
+++ Classes/constructors.py
@@ -0,0 +1,11 @@
+class Point:
+
+    def draw(self):
+        print("draw")
+
+
+# In here when we create a point object we want to supply initial value for x an y coordinates
+# Like (1,2)
+point = Point()
+
+print(type(point))
