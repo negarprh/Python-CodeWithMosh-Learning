d36cf9897745b5621a3acb90e99ff4fa4aaa4e59
negarprh <negarpr@hotmail.com>
2024-12-14 21:12:48 -0500

    creating_classes

--- Classes/creating_classes.py
+++ Classes/creating_classes.py
@@ -6,9 +6,20 @@ class Point:
     # In this block we define all the function that are related to point
     # For example: draw point, move point or get distance between two point
 
+    # Functions in class should at least have a parameter
+    # this paramether? self
     def draw(self):
         print("draw")
 
 
-point = Point()
-point.
+point = Point()  # This return a point object
+
+print(type(point))
+# Output: <class '__main__.Point'> ---> Main is the name of our module
+
+# Is Instance: sometimes we have an object and we want to know
+# If this object is an instance of a given class
+# For exemaple in here we check if the point object is an instance of the given class
+# (Object, Instance of Given Class)
+print(isinstance(point, Point))
+# Output: True since point object is an instance of the point class
